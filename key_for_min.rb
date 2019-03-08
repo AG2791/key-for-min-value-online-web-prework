84a112bded3af24d8ad7c84501b41b8f5e116516
AJ Guiles <ashiguiles@gmail.com>
2019-03-08 00:05:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450}
  def key_for_min_value(name_hash)
  
  if 
-   name_hash ={}
+   name_hash =={}
    return nil
  else
   name_hash.sort_by { |key, value| value } 
